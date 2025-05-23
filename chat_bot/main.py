cd55c3cd4f36bce492f32db2c9a4d488c8111adc
Yehor Davydov <yehordavydov@gmail.com>
2025-05-23 13:17:15 +0200

    corrected result

--- chat_bot/main.py
+++ chat_bot/main.py
@@ -1,52 +1,54 @@
-contacts = {}
-
-def parse_input(user_input):
-    parts = user_input.strip().lower().split()
-    cmd = parts[0]
-    args = parts [1:]
-    return cmd, args
-
-def add_contact(args) :
-    if len(args) < 2:
-        print("Enter name and phone number")
-        return
-    name, phone = args[0], args[1]
-    contacts[name] = phone
-    print(f"Contact with name {name} and number {phone} added ")
-
-def change_contact(args):
-    if len(args) < 2 :
-        print("Enter name and new phone number")
-        return
-    name, phone = args[0], args[1]
-
-    if name in contacts:
-        contacts[name] = phone
-        print(f"Phone number for {name} changed to {phone}")
-    else:
-        print(f"Contact with name {name} not found ")
-
-def show_phone(args) :
-    if not args:
-        print("Enter contact name")
-        return
-    name = args[0]
-    phone = contacts.get(name)
-    if phone:
-        print(f"Phone number for {name}: {phone}")
-    else:
-        print(f"Contact with name {name} not found")
-
-def show_all():
-    if not contacts:
-        print("Contact is absent")
-    else:    
-        print("All contacts:")
-    for name, phone in contacts.items():
-        print(f"{name.title()}: {phone}")
 
 def main():
+    contacts = {}
+
+    def parse_input(user_input):
+        parts = user_input.strip().lower().split()
+        cmd = parts[0]
+        args = parts [1:]
+        return cmd, args
+
+    def add_contact(args) :
+        if len(args) < 2:
+            print("Enter name and phone number")
+            return
+        name, phone = args[0], args[1]
+        contacts[name] = phone
+        print(f"Contact with name {name} and number {phone} added ")
+
+    def change_contact(args):
+        if len(args) < 2 :
+            print("Enter name and new phone number")
+            return
+        name, phone = args[0], args[1]
+
+        if name in contacts:
+            contacts[name] = phone
+            print(f"Phone number for {name} changed to {phone}")
+        else:
+            print(f"Contact with name {name} not found ")
+
+    def show_phone(args) :
+        if not args:
+            print("Enter contact name")
+            return
+        name = args[0]
+        phone = contacts.get(name)
+        if phone:
+            print(f"Phone number for {name}: {phone}")
+        else:
+            print(f"Contact with name {name} not found")
+
+    def show_all():
+        if not contacts:
+            print("Contact is absent")
+        else:    
+            print("All contacts:")
+            for name, phone in contacts.items():
+                print(f"{name.title()}: {phone}")
+
     print("Welcome to the assistant bot!")
+    
     while True:
         user_input = input("Enter a command: ")
         command, args = parse_input(user_input)
@@ -67,11 +69,11 @@ def main():
         elif command == "phone":
             show_phone(args)
 
-        elif command == "all":
+        elif command == "show":
             show_all()
 
         else:
-            print("Invalid command.")
+            print("Unknown command. Try: add, change, phone, show, exit")
 
 if __name__ == "__main__":
     main()
