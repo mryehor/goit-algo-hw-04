b1aa84d257f494d0dbbc10d6f54ad022d3f2a0d6
Yehor Davydov <yehordavydov@gmail.com>
2025-05-24 14:19:52 +0200

    add decorator

--- chat_bot/main.py
+++ chat_bot/main.py
@@ -1,3 +1,11 @@
+#Вимоги до завдання:
+
+#Bсі помилки введення користувача повинні оброблятися за допомогою декоратора input_error. 
+#Цей декоратор відповідає за повернення користувачеві повідомлень типу "Enter user name", "Give me name and phone please" тощо.
+#Декоратор input_error повинен обробляти винятки, що виникають у функціях — handler — і це винятки KeyError, ValueError, IndexError. 
+#Коли відбувається виняток декоратор повинен повертати відповідь користувачеві. Виконання програми при цьому не припиняється.
+
+
 
 def main():
     contacts = {}
@@ -7,35 +15,52 @@ def main():
         cmd = parts[0]
         args = parts [1:]
         return cmd, args
+    
+    def input_error(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except ValueError:
+                return "Error: Please enter valid data."
+            except KeyError:
+                return "Error: Contact not found"
+            except IndexError:
+                return "Error: Missing arguments. Please provide enough information."
+        return inner
 
+    @input_error
     def add_contact(args) :
         if len(args) < 2:
-            return "Enter name and phone number"
+            raise IndexError
         name, phone = args[0], args[1]
+        if not phone.isdigit():
+            raise ValueError
         contacts[name] = phone
         return f"Contact with name {name} and number {phone} added "
-
+    
+    @input_error
     def change_contact(args):
         if len(args) < 2 :
-            return "Enter name and new phone number"
+            raise IndexError
         name, phone = args[0], args[1]
-
-        if name in contacts:
-            contacts[name] = phone
-            return f"Phone number for {name} changed to {phone}"
-        else:
-            return f"Contact with name {name} not found "
-
+        if name not in contacts:
+            raise KeyError
+        if not phone.isdigit():
+            raise ValueError
+        contacts[name] = phone
+        return f"Phone number for {name} changed to {phone}."
+    
+    @input_error
     def show_phone(args) :
-        if not args:
-            return "Enter contact name"
+        if len(args) < 1:
+            raise IndexError
         name = args[0]
-        phone = contacts.get(name)
-        if phone:
-            return f"Phone number for {name}: {phone}"
-        else:
-            return f"Contact with name {name} not found"
+        if name not in contacts:
+            raise KeyError
+        return f"Phone number for {name}: {contacts[name]}"
 
+
+    @input_error
     def show_all():
         if not contacts:
             return "Contact list is empty"
@@ -73,7 +98,7 @@ def main():
         elif command == "show":
             result = show_all()
             print(result)
-            
+
         else:
             print("Unknown command. Try: add, change, phone, show, exit")
 
