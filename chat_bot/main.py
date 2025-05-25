57bb303402711c6cbc4756193ad5b8536aa96812
Yehor Davydov <yehordavydov@gmail.com>
2025-05-25 12:30:34 +0200

    changed function main

--- chat_bot/main.py
+++ chat_bot/main.py
@@ -1,82 +1,56 @@
-#Вимоги до завдання:
+def parse_input(user_input):
+    parts = user_input.strip().lower().split()
+    cmd = parts[0]
+    args = parts [1:]
+    return cmd, args
 
-#Bсі помилки введення користувача повинні оброблятися за допомогою декоратора input_error. 
-#Цей декоратор відповідає за повернення користувачеві повідомлень типу "Enter user name", "Give me name and phone please" тощо.
-#Декоратор input_error повинен обробляти винятки, що виникають у функціях — handler — і це винятки KeyError, ValueError, IndexError. 
-#Коли відбувається виняток декоратор повинен повертати відповідь користувачеві. Виконання програми при цьому не припиняється.
+def add_contact(contacts, args) :
+    if len(args) < 2:
+        return "Enter name and phone number (e.g. add Alice 12345)"
+    name, phone = args[0], args[1]
+    contacts[name] = phone
+    return f"Contact with name {name} and number {phone} added "
 
+def change_contact(contacts, args):
+    if len(args) < 2 :
+        return "Enter name and new phone number"
+    name, phone = args[0], args[1]
 
+    if name in contacts:
+        contacts[name] = phone
+        return f"Phone number for {name} changed to {phone}"
+    else:
+        return f"Contact with name {name} not found "
 
+def show_phone(contacts, args) :   
+    if not args:
+        return "Enter contact name"
+    name = args[0]
+    phone = contacts.get(name)
+    if phone:
+        return f"Phone number for {name}: {phone}"
+    else:
+        return f"Contact with name {name} not found"
+
+def show_all(contacts):
+    if not contacts:
+        return "Contact list is empty"
+    else:    
+        result = ["All contacts: "]
+        for name, phone in contacts.items():
+            result.append(f"{name.title()}: {phone}")
+        return "\n".join(result)
+        
 def main():
     contacts = {}
 
-    def parse_input(user_input):
-        parts = user_input.strip().lower().split()
-        cmd = parts[0]
-        args = parts [1:]
-        return cmd, args
-    
-    def input_error(func):
-        def inner(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except ValueError:
-                return "Error: Please enter valid data."
-            except KeyError:
-                return "Error: Contact not found"
-            except IndexError:
-                return "Error: Missing arguments. Please provide enough information."
-        return inner
-
-    @input_error
-    def add_contact(args) :
-        if len(args) < 2:
-            raise IndexError
-        name, phone = args[0], args[1]
-        if not phone.isdigit():
-            raise ValueError
-        contacts[name] = phone
-        return f"Contact with name {name} and number {phone} added "
-    
-    @input_error
-    def change_contact(args):
-        if len(args) < 2 :
-            raise IndexError
-        name, phone = args[0], args[1]
-        if name not in contacts:
-            raise KeyError
-        if not phone.isdigit():
-            raise ValueError
-        contacts[name] = phone
-        return f"Phone number for {name} changed to {phone}."
-    
-    @input_error
-    def show_phone(args) :
-        if len(args) < 1:
-            raise IndexError
-        name = args[0]
-        if name not in contacts:
-            raise KeyError
-        return f"Phone number for {name}: {contacts[name]}"
-
-
-    @input_error
-    def show_all():
-        if not contacts:
-            return "Contact list is empty"
-        else:    
-            result = ["All contacts: "]
-            for name, phone in contacts.items():
-                result.append(f"{name.title()}: {phone}")
-            return "\n".join(result)
-
     print("Welcome to the assistant bot!")
-    
+
     while True:
         user_input = input("Enter a command: ")
         command, args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
+        if command in ["exit", "close"]:
             print("Good bye!")
             break
 
@@ -84,21 +58,21 @@ def main():
             print("How can I help you?")
 
         elif command == "add":
-            result = add_contact(args)
+            result = add_contact(contacts, args)
             print(result)
 
         elif command == "change":
-            result = change_contact(args)
+            result = change_contact(contacts, args)
             print(result)
 
         elif command == "phone":
-            result = show_phone(args)
+            result = show_phone(contacts, args)
             print(result)
 
         elif command == "show":
-            result = show_all()
+            result = show_all(contacts)
             print(result)
-
+            
         else:
             print("Unknown command. Try: add, change, phone, show, exit")
 
